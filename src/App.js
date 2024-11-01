1e836123fd505985c6f88429e64434952c2a766d
kareem gaber <kgaber985@gmail.com>
2024-11-01 16:20:19 +0200

    Configure React Router basename for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -21,26 +21,24 @@
 // export default App;
 
 
-
-
-
-// src/App.js
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProductDetailPage from './pages/ProductDetailPage';
 import CartPage from './pages/CartPage';
+import { CartProvider } from './context/CartContext';
+import './styles/styles.css';
 
-function App() {
-  return (
-    <BrowserRouter basename="/React_assessment-">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/product/:id" element={<ProductDetailPage />} />
-        <Route path="/cart" element={<CartPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const App = () => (
+    <CartProvider>
+        <Router basename="/React_assessment-">
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/product/:id" element={<ProductDetailPage />} />
+                <Route path="/cart" element={<CartPage />} />
+            </Routes>
+        </Router>
+    </CartProvider>
+);
 
 export default App;
