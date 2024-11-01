ec8f6a883eaa390279647aa8c7dcdc5445ea574e
kareem gaber <kgaber985@gmail.com>
2024-11-01 16:12:39 +0200

    Update README

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import HomePage from './pages/HomePage';
+// import ProductDetailPage from './pages/ProductDetailPage';
+// import CartPage from './pages/CartPage';
+// import { CartProvider } from './context/CartContext';
+// import './styles/styles.css';
+
+// const App = () => (
+//     <CartProvider>
+//         <Router>
+//             <Routes>
+//                 <Route path="/" element={<HomePage />} />
+//                 <Route path="/product/:id" element={<ProductDetailPage />} />
+//                 <Route path="/cart" element={<CartPage />} />
+//             </Routes>
+//         </Router>
+//     </CartProvider>
+// );
+
+// export default App;
+
+
+
+
+
+// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProductDetailPage from './pages/ProductDetailPage';
 import CartPage from './pages/CartPage';
-import { CartProvider } from './context/CartContext';
-import './styles/styles.css';
 
-const App = () => (
-    <CartProvider>
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/product/:id" element={<ProductDetailPage />} />
-                <Route path="/cart" element={<CartPage />} />
-            </Routes>
-        </Router>
-    </CartProvider>
-);
+function App() {
+  return (
+    <BrowserRouter basename="/React_assessment-">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductDetailPage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
